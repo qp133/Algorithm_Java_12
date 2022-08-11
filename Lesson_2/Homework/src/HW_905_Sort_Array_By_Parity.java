69bf1f801bfc35cf3312a676a3a5e8442eacdbda
qp133 <quangphung133@gmail.com>
2022-08-12 00:05:56 +0700

    update

---
+++ Lesson_2/Homework/src/HW_905_Sort_Array_By_Parity.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class HW_905_Sort_Array_By_Parity {
+    public static int[] sortArrayByParity(int[] nums) {
+        int i = 0;
+        int j = nums.length-1;
+
+        while (i < j) {
+            if (nums[i] % 2 > nums[j] % 2) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            if (nums[i] % 2 == 0) {
+                i++;
+            }
+            if (nums[j] % 2 == 1) {
+                j--;
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        sortArrayByParity(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
